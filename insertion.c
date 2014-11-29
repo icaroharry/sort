c931aa01b69ca16bca841961ae3034dfbbb63091
Ícaro Harry <icaropc17@gmail.com>
2014-11-29 20:02:17 -0200

    correcting number_of_swaps, insertion method

--- insertion.c
+++ insertion.c
@@ -1,14 +1,20 @@
-void insertion_sort(int *vet, int tam) {
-   int i, j, eleito;
-   for (i = 1; i < tam; i++){
-      eleito = vet[i];
+/**
+ * Insertion sort implementation
+ * @complexity O(n²) 
+ * @param int* array Array that will be sorted
+ * @param int  size Size of the array
+ */
+void insertion_sort(int *array, int size) {
+   int i, j, selected;
+   for (i = 1; i < size; i++){
+      selected = array[i];
       j = i - 1;
-      while ((j>=0) && (eleito < vet[j])) {
-         vet[j+1] = vet[j];
+      while ((j >= 0) && (selected < array[j])) {
+         array[j+1] = array[j];
          j--;
          number_of_comparisons++;
-         number_of_swaps++;
       }
-      vet[j+1] = eleito;
+      number_of_swaps++;
+      array[j+1] = selected;
    }
 }
